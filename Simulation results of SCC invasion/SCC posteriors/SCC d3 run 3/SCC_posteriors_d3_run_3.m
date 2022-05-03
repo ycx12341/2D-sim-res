5e6d2139fe1d52fa0f339277e42690d4f9eaf291
ycx12341 <58596111+ycx12341@users.noreply.github.com>
2022-05-03 16:21:47 +0100

    Update SCC_posteriors_d3_run_3.m

--- Simulation results of SCC invasion/SCC posteriors/SCC d3 run 3/SCC_posteriors_d3_run_3.m
+++ Simulation results of SCC invasion/SCC posteriors/SCC d3 run 3/SCC_posteriors_d3_run_3.m
@@ -1,8 +1,8 @@
-% Post_process.m
+% SCC_posteriors_d3_run_3.m
 % Author: Yunchen Xiao
 % This MATLAB file generates the plots of probability densities of the
 % parameter estimations at the end of each round of the second run of 
-% applying ABC scheme on the post-day 6 pattern of the SCC reference 
+% applying ABC scheme on the post-day 3 pattern of the SCC reference 
 % dataset.
 
 %% Environment settings
