e7e772eff84244b83f84d131d2d66259d423748a
ycx12341 <58596111+ycx12341@users.noreply.github.com>
2022-05-03 16:22:28 +0100

    Update SCC_posteriors_d6_run_2.m

--- Simulation results of SCC invasion/SCC posteriors/SCC d6 run 2/SCC_posteriors_d6_run_2.m
+++ Simulation results of SCC invasion/SCC posteriors/SCC d6 run 2/SCC_posteriors_d6_run_2.m
@@ -1,4 +1,4 @@
-% Post_process.m
+% SCC_posterior_d6_run2.m
 % Author: Yunchen Xiao
 % This MATLAB file generates the plots of probability densities of the
 % parameter estimations at the end of each round of the second run of 
