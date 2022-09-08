270ed61f0abbc2751192d3a97a0d120bffd7b838
Tian Z <ecburx@burx.vip>
2022-09-08 21:05:19 +0100

    C++: Sample:SCC

--- cpp_src/sample/scc.cpp
+++ cpp_src/sample/scc.cpp
@@ -3,48 +3,60 @@
 /* Terminate simulation if the decrease in averaged Least Square Differences has dropped below 5%. */
 #define TERMINATE_CONDITION 0.05
 
+constexpr static const unsigned X = SCC_X_LEN;
+constexpr static const unsigned Y = SCC_Y_LEN;
+constexpr static const unsigned N = DEFAULT_N_DIMS;
+
+/**
+ * Example: TODO
+ */
 void scc_simple() {
-    Parameters p(DEFAULT_N_DIMS);
-    DBL_T      mean_diff;
+    Parameters<N> p;
+    DBL_T         mean_before;
 
     /* Round 1
      *
      */
-    auto scc_r1 = *Sim_2D_Factory::SCC_375(DEFAULT_N_DIMS, SEED);
-    scc_r1.pars->init();
-    p         = scc_r1.simulate(MULTI_THREADING);
-    std::cout << p;
-    auto scc = *Sim_2D_Factory::SCC_375(DEFAULT_N_DIMS, SEED);
-    scc.pars->load(p);
-    p = scc.simulate(MULTI_THREADING);
-    std::cout << "A";
-//    auto scc2 = *Sim_2D_Factory::SCC_375(DEFAULT_N_DIMS, SEED);
-//    *scc2.pars = p;
-//    p = scc2.simulate(MULTI_THREADING);
-//
-//    /* Round 2 ~ Round 3
-//     *
-//     */
-//    for (int i = 2; i <= 3; ++i) {
-//        std::cout << "A";
-//        auto scc = *Sim_2D_Factory::SCC_375(DEFAULT_N_DIMS, SEED);
-//        *scc.pars = p;
-//        p = scc.simulate(MULTI_THREADING);
-//    mean_diff = scc_r1.sum_diff / (DBL_T) scc_r1.nnan_idxs.size();
-//    }
-//
-//    /* Round 4 ~ Round X
-//     *
-//     */
-//    for (int i = 4;; ++i) {
-//        std::cout << "B";
-//        auto scc = *Sim_2D_Factory::SCC(DEFAULT_N_DIMS, SEED);
-//        *scc.pars = p;
-//        p = scc.simulate(MULTI_THREADING);
-//        if ((mean_diff - scc.sum_diff / (DBL_T) scc.nnan_idxs.size())
-//            / mean_diff < TERMINATE_CONDITION) {
-//            break;
-//        }
-//    mean_diff = scc_r1.sum_diff / (DBL_T) scc_r1.nnan_idxs.size();
-//    }
+    auto scc_r1 = *Sim_2D_Factory<N, Y, X>::SCC_375(SEED);
+    scc_r1.pars.init();
+    p = scc_r1.simulate(MULTI_THREADING);
+
+    p.export_csv();
+    scc_r1.export_least_square();
+    scc_r1.export_summary();
+
+    /* Round 2 ~ Round 3
+     *
+     */
+    for (int i = 2; i <= 3; ++i) {
+        auto scc = *Sim_2D_Factory<N, Y, X>::SCC_375(SEED);
+        scc.pars = p;
+        p           = scc.simulate(MULTI_THREADING);
+        mean_before = scc.sum_diff / (DBL_T) scc.nnan_idxs.size();
+
+        p.export_csv();
+        scc_r1.export_least_square();
+        scc_r1.export_summary();
+    }
+
+    /* Round 4 ~ Round X
+     *
+     */
+    for (int i = 4;; ++i) {
+        auto scc = *Sim_2D_Factory<N, Y, X>::SCC(SEED);
+        scc.pars = p;
+        p = scc.simulate(MULTI_THREADING);
+
+        p.export_csv();
+        scc_r1.export_least_square();
+        scc_r1.export_summary();
+
+        DBL_T mean_after = scc.sum_diff / (DBL_T) scc.nnan_idxs.size();
+        if (mean_before - mean_after < TERMINATE_CONDITION * mean_before) {
+            std::cout << "Terminate simulation: the decrease in averaged Least Square Differences has dropped below 5%."
+                      << std::endl;
+            break;
+        }
+        mean_before = mean_after;
+    }
 }
