c2362789f39488b407c86ed3a763b812e4e8e8f2
Tian Z <ecburx@burx.vip>
2022-09-11 11:58:46 +0100

    C++: add comments to collection.h

--- cpp_src/collection/collection.h
+++ cpp_src/collection/collection.h
@@ -1,8 +1,24 @@
+/**
+ * Extended functions for collections.
+ */
+
 #ifndef SIM_2D_CPP_COLLECTION_H
 #define SIM_2D_CPP_COLLECTION_H
 
 #include "matrix.h"
 
+/**
+ * Generate a sequence in an array.
+ * Equivalent to:
+ *      seq(from = from, to = to, length.out = length_out)
+ * @tparam T         type of elements.
+ * @param buf        buffer: a T type array.
+ * @param from       the starting value of the sequence.
+ * @param to         the (maximal) end value of the sequence.
+ * @param length_out desired length of the sequence.
+ * @attention        desired length is not always equal to the size of array.
+ * @return           The number of elements generated in the array.
+ */
 template<typename T>
 int seq_length_out(T *buf, T from, T to, const int length_out) {
     if (length_out <= 0) { return 0; }
@@ -23,6 +39,18 @@ int seq_length_out(T *buf, T from, T to, const int length_out) {
     return length_out;
 }
 
+/**
+ * Generate a sequence in an array.
+ * Equivalent to:
+ *      seq(from = from, to = to, by = by)
+ * @tparam T   type of elements.
+ * @param buf  buffer: a T type array.
+ *             by = ((to - from)/(length.out - 1))
+ * @param from the starting value of the sequence.
+ * @param to   the (maximal) end value of the sequence.
+ * @param by   number: increment of the sequence.
+ * @return     The number of elements generated in the array.
+ */
 template<typename T>
 int seq_by(T *buf, T from, T to, T by) {
     if (from > to) {
@@ -41,6 +69,14 @@ int seq_by(T *buf, T from, T to, T by) {
     return i;
 }
 
+/**
+ * Return indexes of those elements which are equal to specified value.
+ * @tparam T   element type.
+ * @tparam Len length of the std::array.
+ * @param arr  std::array.
+ * @param val  T type value.
+ * @return     A vector of indexes.
+ */
 template<typename T, int Len>
 std::vector<int> std_array_which_equals(const std::array<int, Len> &arr, const T val) {
     std::vector<int> res;
@@ -55,6 +91,14 @@ std::vector<int> std_array_which_equals(const std::array<int, Len> &arr, const T
     return res;
 }
 
+/**
+ * Check if an array contains specified element.
+ * @tparam T   element type.
+ * @param val  T type value.
+ * @param len  length of the array.
+ * @param arr  T type array.
+ * @return     True if this array contains at least one val.
+ */
 template<typename T>
 bool array_contains(T val, const int len, T *arr) {
     for (int i = 0; i < len; ++i) {
@@ -63,6 +107,12 @@ bool array_contains(T val, const int len, T *arr) {
     return false;
 }
 
+/**
+ * Return indexes of those smallest elements in a vector.
+ * @tparam T     element type.
+ * @param vector T type vector.
+ * @return       A vector of indexes.
+ */
 template<typename T>
 std::vector<int> vector_which_min(const std::vector<T> &vector) {
     std::vector<int> mins;
@@ -83,6 +133,12 @@ std::vector<int> vector_which_min(const std::vector<T> &vector) {
     return mins;
 }
 
+/**
+ * Return indexes of those largest elements in a vector.
+ * @tparam T     element type.
+ * @param vector T type vector.
+ * @return       A vector of indexes.
+ */
 template<typename T>
 std::vector<int> vector_which_max(const std::vector<T> &vector) {
     std::vector<int> maxes;
@@ -103,6 +159,14 @@ std::vector<int> vector_which_max(const std::vector<T> &vector) {
     return maxes;
 }
 
+/**
+ * Remove multiple elements from a vector by their indexes.
+ * @tparam T      element type.
+ * @param vector  T type vector.
+ * @param len     Number of indexes.
+ * @param indexes Int array holding indexes of elements to remove.
+ * @return A vector with specified elements removed.
+ */
 template<typename T>
 std::vector<T> vector_remove_many_by_index(const std::vector<T> &vector, const int len, int *indexes) {
     std::vector<T> res;
@@ -114,6 +178,13 @@ std::vector<T> vector_remove_many_by_index(const std::vector<T> &vector, const i
     return res;
 }
 
+/**
+ * Get the minimum value from a <K, DBL_T> std::map.
+ * @tparam K  Key type.
+ * @param map std::map.
+ * @return minimum value in the std::map.
+ *         INFINITY if map is empty.
+ */
 template<typename K>
 DBL_T map_values_min(const std::map<K, DBL_T> &map) {
     DBL_T min = INFINITY;
@@ -123,6 +194,12 @@ DBL_T map_values_min(const std::map<K, DBL_T> &map) {
     return min;
 }
 
+/**Get the maximum value from a <K, DBL_T> std::map.
+ * @tparam K  Key type.
+ * @param map std::map.
+ * @return maximum value in the std::map.
+ *         -INFINITY if map is empty.
+ */
 template<typename K>
 DBL_T map_values_max(const std::map<K, DBL_T> &map) {
     DBL_T max = (DBL_T) -INFINITY;
